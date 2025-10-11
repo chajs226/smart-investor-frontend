337555080943d7972b76ecb15f707d038b1223ce
C110134 <junseong.cha@amc.seoul.kr>
2025-10-12 06:05:55 +0900

    api route  for deploy

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/backend/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        //destination: 'http://localhost:8000/api/:path*',
+        destination: 'https://smart-investor-backend-8xjt.onrender.com/api/:path*',
       },
     ];
   },
